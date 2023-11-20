a3a7abbf836f8633363d6347d1e189f3df675313
User <User@DESKTOP-E4C2K8O>
2023-11-20 11:36:10 +0300

    Добавьте файлы проекта.

---
+++ DZ_c_dies_7711/DataTransfer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace DZ_c_dies_7711
+{
+    public delegate string DDD();
+    public class DataTransfer
+    {
+        public event DDD DataSent;
+        public event DDD DataReceived;
+
+        public void OuEstText(string str)
+        {
+            var f = System.IO.File.ReadAllText(str);
+            DataSent?.Invoke();
+            Console.WriteLine(" отправлено ");
+
+            if (f.Length != 0)
+            {
+                DataReceived?.Invoke();
+                Console.WriteLine(" файл получен ");
+            }
+        }
+        public void JSON(string str, string LISTA1)
+        {
+            string json = JsonConvert.SerializeObject(LISTA1);
+
+            System.IO.File.WriteAllText(str, json);
+            OuEstText(str);
+        }
+        public void XML(string str, string LISTA1)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(string));
+            using (FileStream fstream = new FileStream(str, FileMode.OpenOrCreate))
+            {
+                xml.Serialize(fstream, LISTA1);
+            }
+            OuEstText(str);
+        }
+
+        public void TXT(string str2, string LISTA1)
+        {
+            using (StreamWriter vrrr = new StreamWriter(str2, false))
+            {
+                    StringBuilder strB = new StringBuilder();
+                    for (int j = 0; j < LISTA1.Length; j++)
+                    {
+                        strB.Append(LISTA1[j]);
+                    }
+                    vrrr.WriteLine(strB.ToString());
+            }
+            OuEstText(str2);
+        }
+
+        public void LisJSON(string str)
+        {
+            string json = System.IO.File.ReadAllText(str);
+            Console.WriteLine(JsonConvert.DeserializeObject(json));
+        }
+
+        public void LisXML(string str)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(string));
+            using (FileStream fs = new FileStream(str, FileMode.OpenOrCreate))
+            {
+                var XML = xmlSerializer.Deserialize(fs);
+                Console.WriteLine(XML);
+            }
+
+        }
+
+        public void LisTXT(string str)
+        {
+            Console.WriteLine(System.IO.File.ReadAllText(str));
+        }
+    }
+}
---
+++ DZ_c_dies_7711/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DZ_c_dies_7711
+{
+    internal class Program
+    {
+        public static string str1 = "C:\\Users\\User\\Desktop\\C#\\DZ_c_dies_7711\\DZ_c_dies_7711\\mmm1.txt";
+        public static string str2 = "C:\\Users\\User\\Desktop\\C#\\DZ_c_dies_7711\\DZ_c_dies_7711\\j.json";
+        public static string str3 = "C:\\Users\\User\\Desktop\\C#\\DZ_c_dies_7711\\DZ_c_dies_7711\\t.txt";
+        public static string str4 = "C:\\Users\\User\\Desktop\\C#\\DZ_c_dies_7711\\DZ_c_dies_7711\\xml.xml";
+        public static string LISTA1;
+        async static public void VVV()
+        {
+            using (FileStream fstream = File.OpenRead(str1))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                await fstream.ReadAsync(buffer, 0, buffer.Length);
+                string textFromFile = Encoding.Default.GetString(buffer);
+                //Console.WriteLine(textFromFile);
+                LISTA1 = textFromFile;
+            }
+        }
+        static void Main(string[] args)
+        {
+            VVV();
+            DataTransfer DT=new DataTransfer();
+            DT.JSON(str2, LISTA1);
+            DT.TXT(str3, LISTA1);
+            DT.XML(str4, LISTA1);
+            DT.LisJSON(str2);
+            DT.LisTXT(str3);
+            DT.LisXML(str4);
+        }
+    }
+}
